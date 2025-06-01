7f8c54679e6342ae18555ae3555d72836af37969
Manuel Fernandes <56774557+FireSkills1@users.noreply.github.com>
2025-06-01 18:34:39 +0100

    Delete PL_24_25_Grupo_32-main (1)/PL_24_25_Grupo_32-main directory

--- PL_24_25_Grupo_32-main (1)/PL_24_25_Grupo_32-main/adiciona.pas
+++
@@ -1,7 +0,0 @@
-program Teste;
-var x, y: integer;
-begin
-  x := 5;
-  y := x + 3;
-  writeln(x, y);
-end.
--- PL_24_25_Grupo_32-main (1)/PL_24_25_Grupo_32-main/helloworld.asm
+++
@@ -1,6 +0,0 @@
-// BEGIN PROGRAM HelloWorld
-pushs "Ola, Mundo!"  // push string
-writes
-writeln
-stop
-// END PROGRAM HelloWorld
